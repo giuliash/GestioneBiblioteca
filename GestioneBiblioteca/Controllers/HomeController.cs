a6ceba78c551120c3f74291b76119865c758a0de
tommaso ravaioli <tommaso.ravaioli@itiscuneo.eu>
2025-05-21 13:20:39 +0200

    Layout2

--- GestioneBiblioteca/Controllers/HomeController.cs
+++ GestioneBiblioteca/Controllers/HomeController.cs
@@ -23,6 +23,17 @@ namespace GestioneBiblioteca.Controllers
             return View();
         }
 
+        public IActionResult Libri()
+        {
+            return View();
+        }
+
+       
+        public IActionResult Prestiti()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
