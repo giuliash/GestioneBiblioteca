7c8a341958f0899a4a65b1822317de76a7346e7c
tommaso ravaioli <tommaso.ravaioli@itiscuneo.eu>
2025-05-21 12:56:23 +0200

    Layout

--- GestioneBiblioteca/wwwroot/js/site.js
+++ GestioneBiblioteca/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// JavaScript personalizzato per la gestione biblioteca
+$(function () {
+    console.log("App Biblioteca caricata.");
+});
