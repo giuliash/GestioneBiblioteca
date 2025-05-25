6fddd321cb3efee185c2fc55e99184cd7045c66f
giulia shestani <xhulia.shestani@itiscuneo.eu>
2025-05-25 21:27:34 +0200

    model(2) Utenti e model(3) Prestiti

---
+++ GestioneBiblioteca/Migrations/20250525191807_AggiuntaPrestitiUtenti2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestioneBiblioteca.Migrations
+{
+    /// <inheritdoc />
+    public partial class AggiuntaPrestitiUtenti2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Utenti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeUtente = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    NumeroTessera = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Sospeso = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utenti", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestiti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataInizio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFine = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    LibroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestiti", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prestiti_Libro_LibroId",
+                        column: x => x.LibroId,
+                        principalTable: "Libro",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Prestiti_Utenti_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Utenti",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestiti_LibroId",
+                table: "Prestiti",
+                column: "LibroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestiti_UserId",
+                table: "Prestiti",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Utenti_Email",
+                table: "Utenti",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Utenti_NumeroTessera",
+                table: "Utenti",
+                column: "NumeroTessera",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestiti");
+
+            migrationBuilder.DropTable(
+                name: "Utenti");
+        }
+    }
+}
---
+++ GestioneBiblioteca/Models/Prestito.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestioneBiblioteca.Models
+{
+    public class Prestito
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La data di inizio è obbligatoria")]
+        [Display(Name = "Data Inizio Prestito")]
+        public DateTime DataInizio { get; set; }
+
+        [Display(Name = "Data Fine Prestito")]
+        public DateTime? DataFine { get; set; }
+
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual Utenti User { get; set; }
+
+        [Required]
+        public int LibroId { get; set; }
+
+        [ForeignKey("LibroId")]
+        public virtual Libro Libro { get; set; }
+
+
+    }
+}
